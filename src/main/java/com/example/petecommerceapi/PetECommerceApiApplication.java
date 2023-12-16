a880768f126a5e1f1e4cba6c47d150c5957261d3
CamilaCerqueira <camilaroberia@gmail.com>
2023-12-16 09:16:23 -0300

    Initial commit

---
+++ src/main/java/com/example/petecommerceapi/PetECommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.petecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetECommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetECommerceApiApplication.class, args);
+	}
+
+}
